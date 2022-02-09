4395b650d25f1f123927e4b76c2f0368cbe1d1cd
shuaib.anwar <shuaib.mbd@gmail.com>
2022-02-09 10:18:56 +0000

    Base code structure

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# swagger-yaml
+About
+=====
+
+Project showing how to setup Swagger for a REST API develop with Node.js, Express and YAML.
+
+Installation
+============
+
+    npm install
+    npm start
+
+Swagger documentation
+===================================
+Navigate to:
+ 
+    http://localhost:3000/api-docs/
+
---
+++ server.js
@@ -0,0 +1,53 @@
+/* ....*/
+const swaggerUi = require ('swagger-ui-express');
+const YAML = require( 'yamljs' );
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.route('/book')
+  .get(function (req, res) {
+    res.send('Get a random book')
+  });
+
+  app.route({
+    method: 'GET',                            
+    path: '/api/v1/sessions/_latest',        
+                                            
+     handler(req) {                      
+      res.send('Get a random book');
+    }
+  });
+ 
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
+
+/* ...
+
+Step 1.
+npm install --save swagger-ui-express
+npm install --save yamljs
+
+Step 2.
+
+Copy line 2 & 3 in existing server.js
+Copy line 15 in existing server.js
+
+Step 3.
+Copy swagger.yaml into project root
+
+Step 4.
+Update swagger.yaml as per your need
+
+ */
